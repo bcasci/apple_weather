7ff3aa2e1c691a67d1322c8435c91b4d33f8c2f4
Brandon Casci <brandon.casci@gmail.com>
2024-01-26 16:14:39 -0500

    WIP

--- app/controllers/weather_forecasts_controller.rb
+++ app/controllers/weather_forecasts_controller.rb
@@ -9,6 +9,9 @@ class WeatherForecastsController < ApplicationController
 
   # Create action for creating weather forecast
   def create
+    # This is potentially a long running operation, so we want to run it in a background job.
+    # The UI could be updated with a simple polling mechanism that checks the status of the job,
+    # or we could use ActionCable to push updates to the UI.
     @weather_forecast_command = WeatherForecastCommand.create(**weather_forecast_params)
 
     render :show, status: :unprocessable_entity unless @weather_forecast_command.success?
